148c2c53e0a57135eecf3cf91735ff35d183ff69
WilliamJYi <yiwilliam1098@gmail.com>
2023-01-18 19:03:46 -0500

    isMoveValid wip

--- client/pieces/pawn.py
+++ client/pieces/pawn.py
@@ -1,18 +1,32 @@
-from piece.py import Piece
+from piece import Piece
 
 class Pawn(Piece):
-    def __init__(self, color, typeOfPiece, position):
-        super().__init__(color, typeOfPiece, position)
-
-
-    
-    def move(self, newPos):
+    def __init__(self, color, typeOfPiece, pos):
+        super().__init__(color, typeOfPiece, pos)
+        self.pos = pos
+        self.color = color
 
         #current position
         #new position
         #if new position is valid then current position = new pos
 
     def isMoveValid(self, newPos):
+        #self.pos = (6, 1) (B, 2)
+        startPos = {"b": 2, "w": 6}
+        if self.color == "b":
+            diff = newPos[0] - self.pos[0]
+            if diff < 0:
+                return False
+            else:
+                return 0
+
+
+        if self.pos[0] == startPos[self.color]:
+            if self.color == "b":
+                return 0
+
+
+
 
 
     
